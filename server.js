852e2cbb1cf3f3011b1f9ece3b9eb8c0100d6d9f
Anshul Anand <ansh.anand@gmail.com>
2019-10-21 02:10:06 -0400

    issue with post request

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res, db) => { profile.handleProfile(req, res, db) })
 app.put('/image', (req, res, db) => { image.handleImage(req, res, db) })
-app.put('/imageurl', (req, res) => { image.handleApiCall(req, res) })
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`app is running on PORT ${process.env.PORT}`);
