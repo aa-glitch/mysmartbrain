5be840167fbbc39a27cbecd3bf41168a48f5232d
Anshul Anand <ansh.anand@gmail.com>
2019-10-21 02:20:07 -0400

    add catch

--- controllers/image.js
+++ controllers/image.js
@@ -20,6 +20,7 @@ const handleImage = (req, res, db) => {
     .then(entries => {
       res.json(entries[0]);
     })
+    .catch(err => res.status(400).json('unable to get entries'))
 }
 
 module.exports = {
