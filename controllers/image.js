5525bb90f97302522247d433a0aa0d4b491fee59
Anshul Anand <ansh.anand@gmail.com>
2019-10-21 01:42:55 -0400

    refactoring

---
+++ controllers/image.js
@@ -0,0 +1,28 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+  apiKey: 'd4e27f3ddb9a4e5484b4915a38727102'
+});
+
+const handleApiCall = (req, res) => {
+  app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+  .then(data => {
+    res.json(data);
+  })
+  .catch(err => res.status(400).json('unable to work with api'))
+}
+
+const handleImage = (req, res, db) => {
+  const { id } = req.body;
+  db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+      res.json(entries[0]);
+    })
+}
+
+module.exports = {
+  handleImage,
+  handleApiCall
+}
